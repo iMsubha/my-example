5d5fb88cbe84652c936a7d2e2c2aae9a1c456188
Subha Fairuz <iMsubha>
2020-09-03 03:23:26 +0600

    Updates

---
+++ precache-manifest.df27f7571c13b477a32f3bb24713f419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5190611cd8d94c9cbc6f117eec43602e",
+    "url": "/my-example/index.html"
+  },
+  {
+    "revision": "464a2788fb785c4bc0be",
+    "url": "/my-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdc2051444cecf1babb9",
+    "url": "/my-example/static/js/2.5ec18d0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-example/static/js/2.5ec18d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464a2788fb785c4bc0be",
+    "url": "/my-example/static/js/main.f5f19a37.chunk.js"
+  },
+  {
+    "revision": "ac1ecdeb4fc7c02ffa0b",
+    "url": "/my-example/static/js/runtime-main.e3a672f6.js"
+  }
+]);
